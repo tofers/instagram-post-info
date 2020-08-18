7208fc75edde42270b651cb99b6385b196eedd52
tofers <tofers90>
2020-08-18 07:13:42 +0300

    init lib InstagramPost

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# instagram-post-info
+# Instagram Post Info
 Instagam post info  - nicname, image, date, likes, location
+
+Getting data about a post on Instagram. Nickname, location, post date, number of likes, photo.
+
+Only links of public posts from Instagram will work, if this is a public account, the result will be an error.
+
+## Installation
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run:
+
+```bash
+composer require tofers/instagram-post-info
+```
+
+or add
+
+```bash
+"tofers/instagram-post-info": "*"
+```
+
+to the require section of your `composer.json` file.
+
+Usage
+-----
+
+Set link to extension in your view:
+
+```php
+<?php
+
+use tofers\instagrampost\InstagramPostInfo;
+
+$instagram_post = new InstagramPostInfo("https://www.instagram.com/p/CB7KzC6BPIn/"); 
+
+echo 'instagram link: ' . $instagram_post->src;
+echo '<br>';
+echo '<br>';
+echo 'Account name: ' . $instagram_post->account;
+echo '<br>';
+echo '<br>';
+echo 'Image src: ' . $instagram_post->image;
+echo '<br>';
+echo '<br>';
+echo 'Date post: ' . $instagram_post->date;
+echo '<br>';
+echo '<br>';
+echo 'Like post: ' . $instagram_post->like;
+echo '<br>';
+echo '<br>';
+echo 'Location: ' . $instagram_post->location;
+
+
+?>
---
+++ lib/InstagramPost.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace tofers\instagrampost;
+/**
+ * Class InstagramPostInfo
+ * @package tofers\instagrampost
+ *
+ *
+ * @property string $src
+ * @property string $account
+ * @property string $location
+ * @property string $image
+ * @property string $date
+ * @property integer $like
+ *
+ * @property string $error
+ */
+class InstagramPostInfo
+{
+    public $src;
+    public $account;
+    public $location;
+    public $image;
+    public $date;
+    public $like;
+
+    public $error;
+
+    public function __construct($src)
+    {
+        if (strpos($src, "embed") === false) {
+            if (substr($src, -1) != "/") {
+                $src .= "/";
+            }
+            $src .= "embed/";
+        }
+        $instagram_post = $src;
+        $this->src = $instagram_post;
+        $c_instagram = curl_init($instagram_post);
+        curl_setopt($c_instagram, CURLOPT_RETURNTRANSFER, 1);
+        $instagram_content = curl_exec($c_instagram);
+        $instagram_code = curl_getinfo($c_instagram, CURLINFO_HTTP_CODE);
+
+        if ($instagram_code != 200) {
+            $this->error = $instagram_code;
+            return false;
+        }
+
+        //$instagram_content = file_get_contents($instagram_post);
+
+        $doc = new \DOMDocument();
+        @$doc->loadHTML($instagram_content);
+
+        $xpath = new \DOMXpath($doc);
+        $images = $xpath->query('//img[@class="EmbeddedMediaImage"]');
+
+        // all links in .blogArticle
+        $image_src = false;
+        foreach ($images as $container) {
+            if (!$image_src) {
+                $image_src = $container->getAttribute("src");
+                $this->image = $image_src;
+            }
+        }
+
+        $accounts = $xpath->query('//span[@class="UsernameText"]');
+        $account = false;
+        foreach ($accounts as $container) {
+            if (!$this->account) {
+                $this->account = trim(preg_replace("/[\r\n]+/", " ", $container->nodeValue));
+            }
+        }
+        $locations = $xpath->query('//a[@class="Location"]');
+        foreach ($locations as $container) {
+            if (!$this->location) {
+                $this->location = trim(preg_replace("/[\r\n]+/", " ", $container->nodeValue));
+            }
+        }
+
+        $likes = $xpath->query('//div[@class="SocialProof"]');
+        foreach ($likes as $container) {
+            $arr = $container->getElementsByTagName("a");
+            foreach ($arr as $item) {
+                $like = trim(preg_replace("/[\r\n]+/", " ", $item->nodeValue));
+                $this->like = preg_replace("/[^0-9]/", '', $like);
+            }
+        }
+        if ($image_src) {
+            $image_headers = get_headers($image_src);
+            foreach ($image_headers as $image_header) {
+                $dh = explode(':', $image_header);
+                if ($dh[0] == "Last-Modified" && isset($dh[1])) {
+                    $this->date = str_ireplace("Last-Modified:", '', $image_header);
+                }
+            }
+        }
+        return true;
+    }
+}
